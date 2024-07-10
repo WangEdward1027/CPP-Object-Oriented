dc05e4d9454f935fd160c1e3f22beecd567a1b42
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-10 23:27:32 +0800

    static_cast：进行合理的类型转换，不合理的转换会报错

--- 强制类型转换/static_cast.cpp
+++ 强制类型转换/static_cast.cpp
@@ -5,27 +5,28 @@ using std::endl;
 
 //1.static_cast
 void test1(){
-    //变量之间类型转换
+    //1.变量之间类型转换
     double a = 3.14;
     int b = static_cast<int>(a);
     cout << a << endl << b << endl;
     
-    //指针之间类型转换:void* -> int*
+    //2.指针之间类型转换
+    //(1)void* -> int* 可以转换
     void * pVoid = malloc(sizeof(int));
     int * pInt = static_cast<int*>(pVoid); //pVoid->pInt,接管刚才申请的堆空间
     *pInt = 2204;
     cout << *pInt << endl;
     free(pInt);
 
-    //char* -> int *不被允许
+    //(2)char* -> int *不被允许
     char* pstr = new char('H');
     int* pInt2 = (int*)pstr; //C风格强转,即使不合理也能成功
     cout << *pInt2 << endl; //H的ASCII值是72,按int型输出
-    //int pInt3 = static_cast<int*>(pstr); //error
+    /* int pInt3 = static_cast<int*>(pstr); //error */
     
-    //const cahr*->char*不被允许
+    //(3)const cahr*->char*不被允许
     const char * pstr2 = "HELLO";
-    //char * pstr3 = static_cast<char*>(pstr2);  //error
+    /* char * pstr3 = static_cast<char*>(pstr2);  //error */
 }
 
 int main()
