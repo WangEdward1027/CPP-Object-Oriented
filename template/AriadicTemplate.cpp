4a023a6c933c068db892772cd5327fe324095512
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-12 10:27:42 +0800

    可变参数模板

--- template/AriadicTemplate.cpp
+++ template/AriadicTemplate.cpp
@@ -13,13 +13,27 @@ void display(Args ...args)//args 函数参数包
     cout << "sizeof...(args) = " << sizeof...(args) << endl;
 }
 
+//普通函数作为递归出口
+void print(){
+    cout << endl;
+}
+
+template <class T,class ...Args>
+void print(T x, Args ...args){
+    cout << x << " ";
+    print(args...); //省略号在包的右边表示解包
+}
+
 void test0(){
     display();
     display(1,"hello",3.3,true);
     display(1,"hello",3.3,true,'a',2);
 }
 
-int main(void){
-    test0();
-    return 0;
+void test1(){
+    print(1,"hello",3.4,true,false,200);
 }
+
+int main(void){
+    test1();
+   
