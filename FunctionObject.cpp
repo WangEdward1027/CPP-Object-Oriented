335f5949896c8e781da63b64545882d40647783f
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-15 21:34:05 +0800

    函数对象

--- FunctionObject.cpp
+++ FunctionObject.cpp
@@ -9,18 +9,18 @@ class FunctionObject{
 public:
     void operator() (){
         cout << "void operator() ()" << endl;
-        _cnt++;
+        ++_cnt;
     }
     
     int operator() (int x){
         cout << "int operator()(int x)" << endl;
-        _cnt++;
+        ++_cnt;
         return x;
     }
 
     double operator() (double x, int y){
         cout << "double operator() (double x, int y)" << endl;
-        _cnt++;
+        ++_cnt;
         return x + y;
     }
     
@@ -38,6 +38,8 @@ void test(){
     fo.operator()(); //本质
     fo(3);
     fo(3.5,3);
+    //如果使用普通函数,则记录函数调用次数只能通过全局变量。不安全,可以被任意篡改
+    //使用函数对象,调用次数设为私有数据成员,无法在类外被修改
     cout << "函数对象被调用的次数:" << fo.getCount() << endl;
 }
 
